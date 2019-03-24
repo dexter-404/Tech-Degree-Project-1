80465f28b7cbba81349e8bb706ec5daf2cea666d
dexter-404 <dexter404@countermail.com>
2019-03-24 16:15:23 +0200

    Added random bg-colour

--- js/script.js
+++ js/script.js
@@ -10,31 +10,36 @@ var quotes = [
     quote: 'Its not the load that breaks you down, its the way you carry it.',
     source: 'www.goodreads.com',
     citation: 'Lou Holtz',
-    year: 'XXXX'
+    year: 'Not 2019',
+    category: 'Inspirational'
   },
   { 
     quote: 'Do you want to know who you are? Dont ask. Act! Action will delineate and define you.',
     source: 'www.goodreads.com',
     citation: 'Thomas Jefferson',
-    year: 'YYYY'
+    year: 'Not 2019',
+    category: 'Inspirational'
   },
   { 
     quote: 'The best preparation for tomorrow is doing your best today.',
     source: 'www.brainyquote.com',
     citation: 'H. Jackson Brown, Jr',
-    year: 'ZZZZ'
+    year: 'Not 2019',
+    category: 'Inspirational'
   },
   { 
     quote: 'It is during our darkest moments that we must focus to see the light.',
     source: 'www.brainyquote.com',
     citation: 'Aristotle',
-    year: 'AAAA'
+    year: 'Not 2019',
+    category: 'Inspirational'
   },
   { 
     quote: 'Two things are infinite: the universe and human stupidity; and Im not sure about the universe.',
     source: 'www.goodreads.com',
     citation: 'Albert Einstein',
-    year: 'BBBB'
+    year: 'Not 2019',
+    category: 'Inspirational'
   }
 ]
 
@@ -56,6 +61,9 @@ function printQuote() {
   var outputDiv;
 
   message = '<p class="quote">' + randomQuote.quote + '</p>';
+  message = '<p class="source">' + randomQuote.source;
+
+  console.log(randomQuote.quote);
   
   if (randomQuote.citation) {
     message += '<span class="citation">' + randomQuote.citation + '</span>';
@@ -70,6 +78,19 @@ function printQuote() {
   outputDiv = document.getElementById('quote-box');
   outputDiv.innerHTML = message;
 
+  randomBgColor();
 }
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+
+function randomBgColor() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  
+  document.body.style.background = bgColor;
+  
+}
