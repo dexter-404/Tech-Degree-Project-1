a6bcf121c190d1de013c7ee51ce5abf8d3cb88e0
dexter-404 <dexter404@countermail.com>
2019-03-27 20:10:01 +0200

    Added comments to all the functions

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ project 1 - A Random Quote Generator
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
+/*Array of quotes with object literals - is accessible globally*/
+
 var quotes = [
   { 
     quote: 'Its not the load that breaks you down, its the way you carry it.',
@@ -40,6 +42,8 @@ var quotes = [
   }
 ]
 
+/* Generating a random number to grab a quote object and returns the random quote object */
+
 function getRandomQuote() {
 
   var i;
@@ -51,6 +55,8 @@ function getRandomQuote() {
   return randomQuote;
 }
 
+/* Generating a random color and set the generated color as document background */
+
 function randomBgColor() {
   var x = Math.floor(Math.random() * 256);
   var y = Math.floor(Math.random() * 256);
@@ -60,6 +66,12 @@ function randomBgColor() {
   document.body.style.background = bgColor;
 }
 
+/* 
+'printQuote()' function receives random quote property from 'getRandomQuote()' function and checks for 
+the prescence of additional property and outputs the result to "quote-box" id. 
+Finally it calls 'randomBgColor()' function to change the background colour randomly 
+*/
+
 function printQuote() {
 
   var randomQuote = getRandomQuote();
@@ -68,8 +80,6 @@ function printQuote() {
 
   message += '<p class="quote">' + randomQuote.quote + '</p>';
   message += '<p class="source">' + randomQuote.source;
-  
-  console.log(randomQuote.quote);
   
   if (randomQuote.citation) {
     message += '<span class="citation">' + randomQuote.citation + '</span>';
@@ -90,6 +100,8 @@ function printQuote() {
   
   randomBgColor();
 }
+
+/* Sets '20 seconds' as interval time for every autorefresh, meanwhile it calls 'printQuote()' function */
 
 var intervalID = window.setInterval(printQuote, 20000);
 
